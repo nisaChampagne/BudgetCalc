cd07485d795d00af5dcdc85ea8a94f1c5a25c2ce
Nisa Champagne <nchampag23@gmail.com>
2020-04-11 15:40:45 -0400

    sep form component

--- budgetcalc/src/app/app.module.ts
+++ budgetcalc/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CalcComponent } from './calc/calc.component';
+import { FormComponent } from './form/form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CalcComponent
+    CalcComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
