7dad5061e16ce48496fac1714442ec87c5d45579
Nisa Champagne <nchampag23@gmail.com>
2020-04-13 09:56:13 -0400

    working on functionality

---
+++ budgetcalc/src/shared/models/budget-item.model.ts
@@ -0,0 +1,6 @@
+export class BudgetItem{
+    // or here
+    constructor(public description:string, public amount:number){}// dont have to set them here
+    // just in the constructor with public next to each name
+    // makes them accessible throughout app
+}
